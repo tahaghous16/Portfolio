e41b659046788155a7f872cc11837a4926b98354
Taha Ghous <153850706+tahaghous16@users.noreply.github.com>
2024-10-02 15:45:27 +0500

    Update Work.jsx

--- My-Portfolio/src/Components/Work.jsx
+++ My-Portfolio/src/Components/Work.jsx
@@ -2,26 +2,26 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: "public/Currency converter.jpg",
+    imgSrc: "./Currency converter.jpg",
     title: "Currency Converter App",
     projectLink:
       "https://66cb7a8e2f99f106bf90fdbb--frabjous-starlight-bd8b19.netlify.app/",
   },
   {
-    imgSrc: "public/CarrioMotor.png",
+    imgSrc: "./CarrioMotor.png",
     title: "Carrio Motor App",
 
     projectLink:
       "https://66cb8736882f3d6714c737de--courageous-profiterole-0d5169.netlify.app/",
   },
   {
-    imgSrc: "public/Innovative Solution.png",
+    imgSrc: "./Innovative Solution.png",
     title: "Innovative Solution App",
     projectLink:
       "https://66cb50edbd7394361ce20be7--unique-gnome-ca28eb.netlify.app/",
   },
   {
-    imgSrc: "public/Tic-Tac-Toe.jpg",
+    imgSrc: "./Tic-Tac-Toe.jpg",
     title: "Tic-Tac-Toe Game App",
 
     projectLink:
