45a14601c50a1ee1ab645f4503e8c21665d60aef
Taha Ghous <tahaghous2002@gmail.com>
2025-05-22 15:27:33 +0500

    small change

--- My-Portfolio/src/Components/Header.jsx
+++ My-Portfolio/src/Components/Header.jsx
@@ -8,7 +8,7 @@ let Header = () => {
         <h1>
           <a href="/" className="logo ">
             <img
-              src="./tahaghous1.png"
+              src="./tahaghous3.png"
               width={160}
               height={160}
               alt="Taha Ghous"
